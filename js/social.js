77fa567b95b14d02f2c93597c3dad9b6a93ddc35
fakelbst <myj226@gmail.com>
2013-09-18 00:14:55 +0000

    update socials & add favicon

--- js/social.js
+++ js/social.js
@@ -1,8 +1,18 @@
 var douban = function(){
 
-    var doubanUrl = 'http://api.douban.com/shuo/v2/statuses/user_timeline/wber';
+    var doubanUrl = 'http://api.douban.com/shuo/v2/statuses/user_timeline/wber?alt=xd&callback=?';
 
     $.getJSON(doubanUrl, function(data){
+        console.log(data);
+    });
+}
+
+var tumblr = function(){
+
+    var tumblrUrl = 'http://api.tumblr.com/v2/blog/fakelbst.tumblr.com/posts?callback=?',
+        apiKey = 'sPqRaIY7bBbKgIdD8MutLOAAU038xZKZ10u0p9QtQ0uh7Qc6Rd'
+
+    $.getJSON(tumblrUrl,{api_key: apiKey, limit: 20}, function(data){
         console.log(data);
     });
 }
