3b5c50853272b22572e3603a585e660acae8bfef
mao <myj226@gmail.com>
2014-07-02 16:21:53 +0800

    test scroll

--- assets/coffee/tracks.coffee
+++ assets/coffee/tracks.coffee
@@ -7,13 +7,29 @@
 url = 'http://ws.audioscrobbler.com/2.0/?method=user.getrecenttracks&user=fakelbst&api_key=4dff88a0423651b3570253b10b745b2c&format=json&limit=50&extended=1'
 
 recentTracks = ($scope, $http) ->
-  $http(
-    method: "GET"
-    url: url
-  ).success((data, status, headers, config) ->
-    $scope.datas = data.recenttracks.track
+  $scope.loadImages = ->
+    console.log 'ssssssss'
+    $http(
+      method: "GET"
+      url: url
+    ).success((data, status, headers, config) ->
+      $scope.datas = data.recenttracks.track
+      return
+    ).error (data, status, headers, config) ->
+      return
     return
-  ).error (data, status, headers, config) ->
-    return
+  $scope.loadImages()
   return
+
+@myApp.directive "scroller", ->
+  (scope, elem, attrs) ->
+    rawElement = elem[0]
+    console.log rawElement
+    console.log elem
+    $(window).bind "scroll", ->
+      console.log 33321
+      scope.$apply "loadImages()"  if (rawElement.scrollTop + rawElement.offsetHeight + 5) >= rawElement.scrollHeight
+      return
+
+    return
 
--- javascripts/tracks.js
+++ javascripts/tracks.js
@@ -11,10 +11,29 @@ this.myApp.config([
 url = 'http://ws.audioscrobbler.com/2.0/?method=user.getrecenttracks&user=fakelbst&api_key=4dff88a0423651b3570253b10b745b2c&format=json&limit=50&extended=1';
 
 recentTracks = function($scope, $http) {
-  $http({
-    method: "GET",
-    url: url
-  }).success(function(data, status, headers, config) {
-    $scope.datas = data.recenttracks.track;
-  }).error(function(data, status, headers, config) {});
+  $scope.loadImages = function() {
+    console.log('ssssssss');
+    $http({
+      method: "GET",
+      url: url
+    }).success(function(data, status, headers, config) {
+      $scope.datas = data.recenttracks.track;
+    }).error(function(data, status, headers, config) {});
+  };
+  $scope.loadImages();
 };
+
+this.myApp.directive("scroller", function() {
+  return function(scope, elem, attrs) {
+    var rawElement;
+    rawElement = elem[0];
+    console.log(rawElement);
+    console.log(elem);
+    $(window).bind("scroll", function() {
+      console.log(33321);
+      if ((rawElement.scrollTop + rawElement.offsetHeight + 5) >= rawElement.scrollHeight) {
+        scope.$apply("loadImages()");
+      }
+    });
+  };
+});
