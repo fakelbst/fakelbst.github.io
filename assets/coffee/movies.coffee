b83538057cebf9bf710be8225d20a780208879a6
mao <myj226@gmail.com>
2014-12-25 22:38:46 +0800

    update movie page

--- assets/coffee/movies.coffee
+++ assets/coffee/movies.coffee
@@ -2,6 +2,7 @@ $ = jQuery
 
 bgdetail = ->
   $('#detail').addClass('site-bg')
+  return
 
 $('#detail').on "click", ->
   $(@).removeClass('site-bg')
@@ -12,4 +13,15 @@ $(document).keyup (e) ->
     $('#detail').removeClass('site-bg')
   return
 
+jQuery ($) ->
+  Apikey = 'dbbe52bb8e34a312e6c564b375a159f0'
+  # http://api.themoviedb.org/3/movie/9778?api_key=dbbe52bb8e34a312e6c564b375a159f0
+  $('.movie a').each (i) ->
+    id = $(@).attr 'data-value'
+    url = 'http://api.themoviedb.org/3/movie/' + id + '?api_key=' + Apikey
+    that = $(@)
+    $.getJSON url, (data) ->
+      console.log data
+
+      $(that).parent('section').append '<p>' + data.overview + '</p>'
 
--- javascripts/movies.js
+++ javascripts/movies.js
@@ -3,7 +3,7 @@ var $, bgdetail;
 $ = jQuery;
 
 bgdetail = function() {
-  return $('#detail').addClass('site-bg');
+  $('#detail').addClass('site-bg');
 };
 
 $('#detail').on("click", function() {
@@ -14,4 +14,19 @@ $(document).keyup(function(e) {
   if (e.keyCode === 27 && $('.site-bg').length > 0) {
     $('#detail').removeClass('site-bg');
   }
+});
+
+jQuery(function($) {
+  var Apikey;
+  Apikey = 'dbbe52bb8e34a312e6c564b375a159f0';
+  return $('.movie a').each(function(i) {
+    var id, that, url;
+    id = $(this).attr('data-value');
+    url = 'http://api.themoviedb.org/3/movie/' + id + '?api_key=' + Apikey;
+    that = $(this);
+    return $.getJSON(url, function(data) {
+      console.log(data);
+      return $(that).parent('section').append('<p>' + data.overview + '</p>');
+    });
+  });
 });
