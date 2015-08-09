de282529e5e0767649c490e7f6e81ed98a2f902c
mao <myj226@gmail.com>
2015-08-09 20:01:04 +0800

    update about background dots animation

--- javascripts/about-bg.js
+++ javascripts/about-bg.js
@@ -9,6 +9,8 @@ var mouseX = 0, mouseY = 0;
 var windowHalfX = window.innerWidth / 2;
 var windowHalfY = window.innerHeight / 2;
 
+var geometry;
+
 document.addEventListener( 'mousemove', onDocumentMouseMove, false );
 
 init();
@@ -25,7 +27,8 @@ function init() {
 
     scene = new THREE.Scene();
 
-    var geometry = new THREE.Geometry( );
+    geometry = new THREE.Geometry();
+    geometry.before = [];
 
     var material = new THREE.PointCloudMaterial({
         color: 0x000000,
@@ -36,7 +39,8 @@ function init() {
 
     for ( var i = 0; i < 8000; i ++ ) {
 
-	      var v = new THREE.Vector3();
+	    var v = new THREE.Vector3();
+        geometry.before[i] = [];
 
         var rand1 = Math.random();
         var rand2 = Math.random();
@@ -49,7 +53,9 @@ function init() {
         v.y = radius * Math.sin(theta2);
         v.z = radius * Math.cos(theta2) * Math.cos(theta1);
 
-  		  geometry.vertices.push(v);
+        geometry.before[i].t1 = rand1;
+        geometry.before[i].t2 = rand2;
+  		geometry.vertices.push(v);
 
     }
 
@@ -65,8 +71,7 @@ function init() {
 
     container.appendChild( renderer.domElement );
 
-
-window.addEventListener( 'resize', onWindowResize, false );
+    window.addEventListener( 'resize', onWindowResize, false );
 
 }
 
@@ -100,6 +105,32 @@ function animate() {
 
 
 function render() {
+
+    var vLength = geometry.vertices.length;
+
+    for( i = 0; i < vLength; i++ ){
+      var v = geometry.vertices[i];
+      var b = geometry.before[i];
+
+      var pos1 =  b.t1 + Math.random() * 0.001 - 0.0005;
+      var pos2 =  b.t2 + Math.random() * 0.001 - 0.0005;
+
+      if( pos1 > 1 ) pos1 = 0;
+      if( pos2 > 1 ) pos2 = 0;
+
+      var theta1 = 360 * pos1 * Math.PI / 180;
+      var theta2 = ( 180 * pos2 - 90 ) * Math.PI / 180;
+      var radius = 380;
+
+      v.x = radius * Math.cos(theta2) * Math.sin(theta1);
+      v.y = radius * Math.sin(theta2);
+      v.z = radius * Math.cos(theta2) * Math.cos(theta1);
+
+      b.t1 = pos1;
+      b.t2 = pos2;
+    }
+
+    geometry.verticesNeedUpdate = true;
 
     camera.position.x += ( mouseX - camera.position.x ) * .05;
     camera.position.y += ( - mouseY - camera.position.y ) * .05;
