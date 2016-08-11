7407289915c53bcabd0b15d6bb55d2cc17d61518
mao <myj226@gmail.com>
2016-08-11 22:37:44 +0800

    New index

--- src/main.js
+++ src/main.js
@@ -2,7 +2,8 @@ import Vue from 'vue'
 import Router from 'vue-router'
 import appFooter from './Footer'
 import appHeader from './Header'
-import albumCube from './AlbumCube'
+import index from './Index'
+import albums from './Albums'
 import quotes from './Quotes'
 import style from './style.css'
 
@@ -19,18 +20,29 @@ const layout = Vue.extend({
     }
   },
   template: `<div class={{style.wrapper}}>
-    <app-header></app-header>
+    <svg class={{style.hidden}}>
+      <defs>
+        <!-- From Karen Menezes: https://www.smashingmagazine.com/2015/05/creating-responsive-shapes-with-clip-path/ -->
+        <clipPath id="polygon-clip-rhomboid" clipPathUnits="objectBoundingBox">
+          <polygon points="0 1, 0.3 0, 1 0, 0.7 1" />
+        </clipPath>
+      </defs>
+    </svg>
+    <div class={{style.sidebar}}>
+      <app-header></app-header>
+      <div class={{style.fake}}></div>
+      <app-footer></app-footer>
+    </div>
     <section class={{style['main-content']}}>
       <router-view></router-view>
-    </section>
-    <app-footer></app-footer>`
+    </section>`
 })
 
 const router = new Router()
 
 router.map({
   '/': {
-    component: albumCube
+    component: albums
   },
   '/quotes': {
     component: quotes
