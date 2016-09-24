f8df8cd9aacc419670f5bfed546147c1209d037a
mao <myj226@gmail.com>
2016-09-25 00:09:42 +0800

    Update books page

--- src/Books/index.js
+++ src/Books/index.js
@@ -1,33 +1,46 @@
 import Vue from 'vue'
+import imagesLoaded from 'imagesloaded'
 import style from './style.css'
-import VueResource from 'vue-resource'
+import scrollbar from '../Components/scrollbar'
+import loading from '../Components/loading'
 
-Vue.use(VueResource)
+Vue.component('loading', loading)
+Vue.component('scrollbar', scrollbar)
 
 export default Vue.extend({
   data() {
     return {
       style,
+      loadingReading: true,
+      loadingRead: true,
       reading: [],
-      read: []
+      read: [],
+      scrollValue: 0
     }
   },
   template: `<div class={{style.wrap}}>
       <h3 class={{style.label}}>Reading</h3>
-      <div class={{style.books}}>
+      <loading :visible.sync="loadingReading"></loading>
+      <div class="{{style.books}} {{style.reading}}">
         <div v-for="r in reading" class={{style.book}}>
-          <div class={{style.cover}} v-bind:style="{ backgroundImage: 'url(' + r.book.images.large + ')' }"></div>
+          <a href={{r.book.alt}} target="_blank" title={{r.book.alt_title}}>
+            <div class="{{style.cover}} {{style.init}}" v-bind:style="{ backgroundImage: 'url(' + r.book.images.large + ')' }"></div>
+          </a>
         </div>
       </div>
 
       <h3 class={{style.label}}>Read</h3>
-      <div class={{style.books}}>
+      <loading :visible.sync="loadingRead"></loading>
+      <div class="{{style.books}} {{style.read}}">
         <div v-for="r in read" class={{style.book}}>
-          <div class={{style.cover}} v-bind:style="{ backgroundImage: 'url(' + r.book.images.large + ')' }"></div>
+          <a href={{r.book.alt}} target="_blank" title={{r.book.alt_title}}>
+            <div class="{{style.cover}} {{style.init}}" v-bind:style="{ backgroundImage: 'url(' + r.book.images.large + ')' }"></div>
+          </a>
         </div>
       </div>
-
-    </div>`,
+    </div>
+    <scrollbar :sy.sync="scrollValue" v-show="!loading"></scrollbar>
+    `,
 
   route: {
     activate: function(transition){
@@ -39,6 +52,17 @@ export default Vue.extend({
         }
       }).then((d) => {
         this.reading = d.data.collections
+
+        imagesLoaded( document.querySelector('[class*=__reading___]'), {background: '[class*=__cover___]'},  () => {
+          this.loadingReading = false
+
+          document.querySelectorAll('[class*=__reading___]>div>a>div').forEach( function(el, i){
+            setTimeout(function() {
+              el.classList.remove(style['init'])
+              el.classList.add(style['scale-animation'])
+            }, 20 * i);
+          })
+        })
       })
 
       Vue.http.jsonp("https://api.douban.com/v2/book/user/wber/collections", {
@@ -48,6 +72,19 @@ export default Vue.extend({
         }
       }).then((d) => {
         this.read = d.data.collections
+
+        imagesLoaded( document.querySelector('[class*=__read___]'), {background: '[class*=__cover___]'},  () => {
+          this.loadingRead = false
+
+          document.querySelectorAll('[class*=__read___]>div>a>div').forEach( function(el, i){
+            setTimeout(function() {
+              el.classList.remove(style['init'])
+              el.classList.add(style['scale-animation'])
+            }, 50 * i);
+          })
+        })
+
+
       })
 
       transition.next()
