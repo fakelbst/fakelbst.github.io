ce8d63250bdae4fef2734f7430cc9f811a8cb76a
毛益杰 <myj226@gmail.com>
2018-02-04 00:29:51 +0800

    Update year progress

--- main.js
+++ main.js
@@ -1,7 +1,21 @@
-var startDay = new Date(2018, 0, 1, 0, 0, 0)
-var fullYear = 365 * 24 * 60 * 60 * 1000
-var now = new Date()
+const startDay = new Date(2018, 0, 1, 0, 0, 0)
+const fullYear = 365 * 24 * 60 * 60 * 1000
+let now = new Date()
 
-var timeRemaining = Math.round( (now.getTime() - startDay.getTime()) / fullYear * 10000) / 100
-document.querySelector('.year-progress').style.width = timeRemaining + '%'
+const sDay = [{
+  date: 'February 3, 2018 19:30:00',
+  title: 'The xx'
+}]
 
+const yearProgress = document.querySelector('.year-progress')
+
+let timeRemaining = Math.round( (now.getTime() - startDay.getTime()) / fullYear * 10000) / 100
+yearProgress.style.width = timeRemaining + '%'
+
+for (let i = 0, j = sDay.length; i < j; i++) {
+  let dotEl = document.createElement('span')
+  dotEl.className = 'dot'
+  let dotPercent = Math.round( (new Date(sDay[i].date).getTime() - startDay.getTime()) / (now.getTime() - startDay.getTime()) * 10000) / 100
+  dotEl.style.left = dotPercent + '%'
+  yearProgress.appendChild(dotEl)
+}
