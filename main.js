0fd370886788a1639ec1c25bf06bc6e565ada15b
mao <myj226@gmail.com>
2019-11-17 21:11:52 +0800

    Fix canvas dpi

--- main.js
+++ main.js
@@ -1,16 +1,17 @@
-var can = document.getElementById('my-canvas')
+let dpi = window.devicePixelRatio
+let canvas = document.getElementById('my-canvas')
 
-function resizeCanvas() {
-  can.style.width = window.innerWidth + 'px'
-  setTimeout(function() {
-    can.style.height = window.innerHeight + 'px'
-  }, 0)
+function fixDpi() {
+  let styleHeight = +getComputedStyle(canvas).getPropertyValue('height').slice(0, -2)
+  //get CSS width
+  let styleWidth = +getComputedStyle(canvas).getPropertyValue('width').slice(0, -2)
+  //scale the canvas
+  canvas.setAttribute('height', styleHeight * dpi)
+  canvas.setAttribute('width', styleWidth * dpi)
 }
 
-window.onresize = resizeCanvas
+fixDpi()
 
-resizeCanvas()
-
-var ctx = can.getContext('2d')
-ctx.font = '10px serif'
+var ctx = canvas.getContext('2d')
+ctx.font = '20px serif'
 ctx.fillText('Under Construction', 10, 50)
