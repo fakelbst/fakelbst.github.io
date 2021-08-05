c50a349b8746d57c72cad80ce15144364394f1f0
mao <myj226@gmail.com>
2021-08-05 17:48:42 +0800

    New index

--- README.md
+++ README.md
@@ -1,22 +1,5 @@
 # fakelbst.github.io
 
-> fakelbst's own page
+> personal website
 
-## Build Setup
 
-``` bash
-# install dependencies
-npm install
-
-# serve with hot reload at localhost:8080
-npm run dev
-
-# build for production with minification
-npm run build
-```
-
-# Credits
-- [Last.fm api](http://www.last.fm/api)
-- [Douban api](https://developers.douban.com/)
-- [Instagram api](https://www.instagram.com/developer/)
-- [Layout inspired by ](http://tympanus.net/codrops/2016/09/02/inspiration-navigation-indicators/)
--- main.js
+++ main.js
@@ -1,101 +1,43 @@
-const DPI = window.devicePixelRatio
-const canvas = document.querySelector('canvas')
-
-const styleHeight = +getComputedStyle(canvas).getPropertyValue('height').slice(0, -2)
-const styleWidth = +getComputedStyle(canvas).getPropertyValue('width').slice(0, -2)
-
-// const canvasHeight = styleHeight * DPI
-// const canvasWidth = styleWidth * DPI
-const canvasHeight = 494
-const canvasWidth = 800
-
-function fixDpi() {
-  canvas.setAttribute('height', canvasHeight)
-  canvas.setAttribute('width', canvasWidth)
-}
-fixDpi()
-
-const ctx = canvas.getContext('2d')
-
-let trigleColors = ["#CDCBD2", "#B3ADB9", "#B3ADB9", "#877283", "#735667", "#454655", "#A7A8CD", "#FFDADA", "#DEA3A4", "#474748", "#785B00", "#CFA616"]
-
-let firstAnim = null
-
-function clear() {
-  ctx.clearRect(0, 0, canvasWidth, canvasHeight)
-}
-
-let angle = 0, points = []
-const H = 180
-function createParticles() {
-  let pos = { x: canvasWidth/2, y: canvasHeight/2 - 150 }
-  for (let i = 0; i < 3; i++) {
-    angle += 60;
-    angle = (60 + angle) % 360;
-    for (let ii = 0; ii < H; ii++) {
-      let dotColorIndex = ~~(Math.random() * trigleColors.length)
-      let dotColor = trigleColors[dotColorIndex]
-      pos = getAngle(pos.x, pos.y, 300 + angle, 2);
-      points.push({
-        color: dotColor,
-        x: pos.x,
-        y: pos.y,
-        firstX: pos.x,
-        firstY: pos.y,
-      })
-    }
+const throttle = (fn, sec) => {
+  let timer = null
+  return function() {
+    if (timer) return
+    timer = setTimeout(() => {
+      clearTimeout(timer)
+      timer = null
+      fn.apply(this, arguments)
+    }, sec)
   }
 }
 
-function getAngle(x, y, angle, H) {
-  var radians = angle * (Math.PI / 180);
-  return { x: x + H * Math.cos(radians), y: y + H * Math.sin(radians) }
-}
+(() => {
+  // gsap.registerPlugin(ScrollTrigger)
 
-function drawT() {
-  for(let i=0, j = points.length; i<j; i++) {
-    ctx.beginPath()
-    ctx.fillStyle = points[i].color
-    ctx.arc(points[i].x, points[i].y, 2, 0, 2 * Math.PI)
-    ctx.fill()
-  }
-  ctx.stroke()
-  firstAnim = requestAnimationFrame(update)
-}
-
-function update() {
-  clear()
-  let range = 5
-  for(let i = 0, j = points.length; i<j; i++) {
-    let dx = Math.random() - 0.5
-    let dy = Math.random() - 0.5
-    if (points[i].x > points[i].firstX + range) {
-      dx = -Math.abs(dx)
-    } else if (points[i].x < points[i].firstX - range) {
-      dx = Math.abs(dx)
+  // gsap.to('.container', {
+  //   xPercent: -100, 
+  //   x: () => innerWidth,
+  //   ease: 'none',
+  //   scrollTrigger: {
+  //     trigger: '.container',
+  //     start: 'top top',
+  //     end: () => innerWidth * 3,
+  //     scrub: true,
+  //     pin: true,
+  //     invalidateOnRefresh: true,
+  //     anticipatePin: 1
+  //   }
+  // })
+  let containerX = 0
+  const MAXWIDTH = innerWidth * 3
+  document.querySelector('body').addEventListener('wheel', throttle((event) => {
+    const delta = Math.sign(event.deltaY)
+    console.log(delta, 111111)
+    containerX += delta * 20
+    console.info(containerX, 112)
+    if (containerX < 0 || containerX >= MAXWIDTH) {
+      return
     }
-    if (points[i].y > points[i].firstY + range) {
-      dy = -Math.abs(dy)
-    } else if (points[i].y < points[i].firstY - range) {
-      dy = Math.abs(dy)
-    }
-    points[i].x += dx
-    points[i].y += dy
-  }
-  drawT()
-}
-
-createParticles()
-drawT()
-
-document.querySelector('.first').addEventListener('click', function() {
-  cancelAnimationFrame(firstAnim)
-  var image = new Image()
-  // image.id = "pic"
-  image.src = canvas.toDataURL()
-  this.appendChild(image)
-  canvas.remove()
-  // canvas.style.display = 'none'
-  bbInstance.next()
-})
-
+    console.info(containerX, 3333)
+    document.querySelector('.container').style.transform = `translateX(-${containerX}px)`
+  }, 50))
+})()
