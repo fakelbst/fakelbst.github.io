94e80076c68bf1c021c1b2220c3881a3fbea8b66
mao <myj226@gmail.com>
2022-03-28 22:45:03 +0800

    New index

--- main.js
+++ main.js
@@ -11,33 +11,52 @@ const throttle = (fn, sec) => {
 }
 
 (() => {
-  // gsap.registerPlugin(ScrollTrigger)
-
-  // gsap.to('.container', {
-  //   xPercent: -100, 
-  //   x: () => innerWidth,
-  //   ease: 'none',
-  //   scrollTrigger: {
-  //     trigger: '.container',
-  //     start: 'top top',
-  //     end: () => innerWidth * 3,
-  //     scrub: true,
-  //     pin: true,
-  //     invalidateOnRefresh: true,
-  //     anticipatePin: 1
-  //   }
-  // })
   let containerX = 0
-  const MAXWIDTH = innerWidth * 3
+  const MAXWIDTH = innerWidth
   document.querySelector('body').addEventListener('wheel', throttle((event) => {
     const delta = Math.sign(event.deltaY)
-    console.log(delta, 111111)
     containerX += delta * 20
-    console.info(containerX, 112)
     if (containerX < 0 || containerX >= MAXWIDTH) {
       return
     }
-    console.info(containerX, 3333)
     document.querySelector('.container').style.transform = `translateX(-${containerX}px)`
   }, 50))
+
+
+  const app = new PIXI.Application({
+    autoResize: true,
+    resizeTo: document.querySelector('.container'),
+    backgroundColor: 0x000,
+    resolution: window.devicePixelRatio
+  });
+
+  document.querySelector('.container').appendChild(app.view);
+
+  PIXI.Loader.shared
+	.add('1.svg', '/assets/dot1.svg')
+	.add('2.svg', '/assets/dot2.svg')
+	.add('3.svg', '/assets/dot3.svg')
+  .load(() => {
+  	app.stage.addChild(...Array.from({length: 900}, (_, i) => {
+    	const sprite = PIXI.Sprite.from(`${Math.random() * 3 + 1 | 0}.svg`)
+      sprite.anchor.set(.5)
+      sprite.scale.set(0.09 + Math.random() * 0.12)
+      sprite.rotation = Math.PI * 2 * Math.random()
+      sprite.position.set(
+      	app.screen.width * Math.random(),
+      	app.screen.height * Math.random(),
+      )
+      return sprite
+    }))
+
+    const graphics = new PIXI.Graphics()
+    graphics.beginFill(0x000)
+    const screenWidth = innerWidth
+    const screenHeight = innerHeight
+    const rWidth = 500
+    const rHeight = 660
+    graphics.drawRect(screenWidth - rWidth / 2, (screenHeight - rHeight) / 2 , rWidth, rHeight)
+    app.stage.addChild(graphics)
+  })
+
 })()
